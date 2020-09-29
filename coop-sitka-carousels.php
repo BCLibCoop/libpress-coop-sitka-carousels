f8ee04de385aa28810bcce7ff321e6f28b08fe35
Siteuser@BCLC <schatz@bc.libraries.coop>
2020-09-29 15:39:22 -0700

    Enqueue scripts, styles correctly to curb PHP notice spam in logs.
    Signed-off-by: Siteuser@BCLC <schatz@bc.libraries.coop>

--- coop-sitka-carousels.php
+++ coop-sitka-carousels.php
@@ -32,21 +32,27 @@ add_action('wpmu_new_blog', 'sitka_carousels_new_blog', 10, 6 );
 add_shortcode( 'sitka_carousel', 'sitka_carousels_shortcode');
 
 
-// Add slick javascript to <head> - https://kenwheeler.github.io/slick/
-wp_enqueue_script('coop-sitka-carousels-slick-js', plugins_url( 'slick/slick.min.js', __FILE__ ), array('jquery', 'jquery-migrate'), FALSE, TRUE);
+add_action('wp_enqueue_scripts', 'coop_sitka_carousels_enqueue_scripts');
+add_action('wp_enqueue_styles', 'coop_sitka_carousels_enqueue_styles');
+
+function coop_sitka_carousels_enqueue_scripts() {
+	// Add slick javascript to <head> - https://kenwheeler.github.io/slick/
+	wp_enqueue_script('coop-sitka-carousels-slick-js', plugins_url( 'slick/slick.min.js', __FILE__ ), array('jquery', 'jquery-migrate'), FALSE, TRUE);
+}
+
+function coop_sitka_carousels_enqueue_styles() {
+
+	// Add CSS for slick javascript library
+	wp_register_style('coop-sitka-carousels-slick-css', plugins_url( 'slick/slick.css', __FILE__ ), false );
+	wp_register_style('coop-sitka-carousels-slick-theme-css', plugins_url( 'slick/slick-theme.css', __FILE__ ), false );
+	wp_enqueue_style('coop-sitka-carousels-slick-css' );
+	wp_enqueue_style('coop-sitka-carousels-slick-theme-css' );
 
 
-// Add CSS for slick javascript library
-wp_register_style('coop-sitka-carousels-slick-css', plugins_url( 'slick/slick.css', __FILE__ ), false );
-wp_register_style('coop-sitka-carousels-slick-theme-css', plugins_url( 'slick/slick-theme.css', __FILE__ ), false );
-wp_enqueue_style('coop-sitka-carousels-slick-css' );
-wp_enqueue_style('coop-sitka-carousels-slick-theme-css' );
-
-
-// Add CSS for carousel customization
-wp_register_style('coop-sitka-carousels-css', plugins_url( 'css/coop-sitka-carousels.css', __FILE__ ), false );
-wp_enqueue_style('coop-sitka-carousels-css' );
-
+	// Add CSS for carousel customization
+	wp_register_style('coop-sitka-carousels-css', plugins_url( 'css/coop-sitka-carousels.css', __FILE__ ), false );
+	wp_enqueue_style('coop-sitka-carousels-css' );
+}
 
 // Register add_meta_box to provide instructions on how to add a carousel to a Highlight post
 add_action( 'add_meta_boxes', 'coop_sitka_carousels_meta_box_add', 10, 2 );
