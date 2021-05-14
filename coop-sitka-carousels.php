8f2c7214646a4233873a0c19a12f66a9601817fb
Sam Edwards <sam@samedwards.ca>
2021-05-14 14:29:36 -0700

    Version bump

--- coop-sitka-carousels.php
+++ coop-sitka-carousels.php
@@ -17,7 +17,7 @@
  * @wordpress-plugin
  * Plugin Name:       Sitka Carousels
  * Description:       New book carousel generator from Sitka/Evergreen catalogue; provides shortcode for carousels
- * Version:           1.1.0
+ * Version:           1.2.0
  * Network:           true
  * Requires at least: 5.2
  * Requires PHP:      7.0
