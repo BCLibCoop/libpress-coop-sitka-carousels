7d6ec02c5db1cfcf168c57e3859d7590b5f4c9b0
Sam Edwards <sam.edwards@bc.libraries.coop>
2023-03-14 15:10:59 -0700

    Don't init an empty carousel

--- inc/views/shortcode.php
+++ inc/views/shortcode.php
@@ -1,20 +1,24 @@
 <div class="sitka-carousel-container">
-    <div class="sitka-carousel" data-flickity='<?= $flickity_options ?>'>
-        <?php foreach ($results as $row) : ?>
-            <div class="carousel-item">
-                <a href="<?= $row['catalogue_url'] ?>">
-                    <div class="carousel-item-cover">
-                        <img alt="" src="<?= $row['cover_url'] ?>" class="carousel-item-image">
-                        <img alt="" src="<?= $attr['no_cover'] ?>" class="carousel-item-image carousel-item-image-default">
-                    </div>
-                    <div class="carousel-item-info">
-                        <span class="carousel-item-title"><?= $row['title'] ?></span>
-                        <br />
-                        <span class="carousel-item-author"><?= $row['author'] ?></span>
-                    </div>
-                </a>
-                <?php /* <div class="carousel-item-description"><?= $row['description'] ?></div> */ ?>
-            </div>
-        <?php endforeach; ?>
-    </div>
+    <?php if (!empty($results)) : ?>
+        <div class="sitka-carousel" data-flickity='<?= $flickity_options ?>'>
+            <?php foreach ($results as $row) : ?>
+                <div class="carousel-item">
+                    <a href="<?= $row['catalogue_url'] ?>">
+                        <div class="carousel-item-cover">
+                            <img alt="" src="<?= $row['cover_url'] ?>" class="carousel-item-image">
+                            <img alt="" src="<?= $attr['no_cover'] ?>" class="carousel-item-image carousel-item-image-default">
+                        </div>
+                        <div class="carousel-item-info">
+                            <span class="carousel-item-title"><?= $row['title'] ?></span>
+                            <br />
+                            <span class="carousel-item-author"><?= $row['author'] ?></span>
+                        </div>
+                    </a>
+                    <?php /* <div class="carousel-item-description"><?= $row['description'] ?></div> */ ?>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    <?php else : ?>
+        <!-- Could not find any carousel items to display -->
+    <?php endif; ?>
 </div>
